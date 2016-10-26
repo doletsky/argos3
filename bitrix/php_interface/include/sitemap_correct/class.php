81dd6769fb3b912ce2201bf9f655d6e1362a8251
doletsky <doletsky@bk.ru>
2016-10-26 10:11:30 +0300

    sitemap corrector step 3_1

--- bitrix/php_interface/include/sitemap_correct/class.php
+++ bitrix/php_interface/include/sitemap_correct/class.php
@@ -2,11 +2,18 @@
 include $_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/include/sitemap_correct/tune_data.php";
 class SMapCorr
 {
-
     // проверка на необходимость корректировки
     function main(&$arFields)//готовит данные для определения необходимых действий и вызывает их
     {
+        global $tuneData;
         $mArFields=$arFields;
-        file_put_contents($_SERVER["DOCUMENT_ROOT"]."/debugLogData.txt", print_r($mArFields,true).print_r($tuneData,true));
+
+            if(is_array($tuneData['IBLOCK'][$mArFields['IBLOCK_ID']])){
+                $val=$tuneData['IBLOCK'][$mArFields['IBLOCK_ID']];
+                file_put_contents($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/include/sitemap_correct/debugLogData.txt", print_r($mArFields,true).print_r($val,true));
+            }else{
+                file_put_contents($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/include/sitemap_correct/debugLogData.txt", "do not correcting");
+            }
+
     }
 }
